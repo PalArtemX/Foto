cbb182bfc8b449521fbe6adc5dffd524caa51d3d
Artem Palyutin <76967266+PalArtemX@users.noreply.github.com>
2021-09-04 23:47:49 +0300

    Initial Commit

---
+++ FotoJSON/FotoJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotoJSONApp.swift
+//  FotoJSON
+//
+//  Created by Artem Palyutin on 04.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FotoJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
